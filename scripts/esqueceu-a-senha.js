485f61ff21c88384bb54f9b60e6f0fe25350304d
CrisGaius <cristianocris12@hotmail.com>
2023-09-30 15:39:25 -0300

    Página de esqueceu-a-senha (falta as medias queries)

---
+++ scripts/esqueceu-a-senha.js
@@ -0,0 +1,36 @@
+const botaoVoltar = document.querySelector('#caixa-voltar')
+
+const formulario = document.querySelector('form#formulario')
+const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+const inputEmail = document.querySelector('input#email')
+const textoErro = document.querySelector('.texto-erro')
+
+botaoVoltar.addEventListener('click', () => {
+    window.open('../pages/sobre.html', '_self')
+})
+
+
+formulario.addEventListener('submit', function  verificarCampo(e) {
+    e.preventDefault()
+
+    const emailValido = validarEmail()
+
+    if (emailValido) {
+        // formulario.submit()
+        console.log('passou.')
+    }
+})
+
+function validarEmail() {
+    if(!regexEmail.test(inputEmail.value)) {
+        inputEmail.classList.add('erro')
+        textoErro.classList.add('mostrar')
+        return false
+    } else {
+        inputEmail.classList.remove('erro')
+        textoErro.classList.remove('mostrar')
+        return true
+    }
+}
+
+inputEmail.addEventListener('input', validarEmail)
