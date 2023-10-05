119b9d1e0d586725f924ceab4bd251af8716ca71
CrisGaius <cristianocris12@hotmail.com>
2023-10-05 11:57:30 -0300

    adicionando modal de excluir (falta as medias queries)

---
+++ scripts/modal.js
@@ -0,0 +1,12 @@
+const botaoAbrirModal = document.querySelector('button#abrir-modal')
+const fundoModal = document.querySelector('#fundo-modal')
+
+botaoAbrirModal.addEventListener('click', () => {
+    fundoModal.classList.add('open')
+})
+
+fundoModal.addEventListener('click', (e) => {
+    if(e.target.id === 'fundo-modal' || e.target.id === 'botao-fechar-modal') {
+        fundoModal.classList.remove('open')
+    }
+})
