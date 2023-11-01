1e38b630f2d4096fc222148af70a786211423609
CrisGaius <cristianocris12@hotmail.com>
2023-11-01 15:06:43 -0300

    Adicionando mail no arquivo de funções úteis e adicionando o back do formulário de voluntário

--- lib/funcoes_uteis.php
+++ lib/funcoes_uteis.php
@@ -40,3 +40,38 @@ function formatar_conta($conta) {
 function formatar_numero($contato) {
     return preg_replace('/(\d{2})(\d{4,5})(\d{4})/', '$1 $2-$3', $contato);
 }
+
+# projeto usando .env também seria uma boa
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+function enviar_email($destinario, $assunto, $mensagem_html) {
+    require_once '../vendor/autoload.php';
+
+    $email = new PHPMailer(true);
+    $email->isSMTP();
+    $email->SMTPDebug = 2;
+    $email->Host = 'smtp.office365.com';
+    $email->Port = 587;
+    $email->SMTPAuth = true;
+    $email->Username = 'doape.testes@outlook.com';
+    $email->Password = 'Pixo_2023@';
+
+    $email->SMTPSecure = 'tls';
+    $email->isHTML(true);
+    $email->CharSet = "UTF-8";
+
+    $email->setFrom("cristianocris12@hotmail.com", "Doa PE");
+    $email->addAddress($destinario);
+    $email->Subject = $assunto;
+
+    $email->Body = $mensagem_html;
+
+    if($email->send()) {
+        return true;
+    } else {
+        return false;
+    }
+
+    $email->smtpClose();
+}
